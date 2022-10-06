8d809d2563821a4ffd76cfde60d772b3feb8c5d7
aniskovich <m.a.aniskovich@gmail.com>
2022-10-06 14:20:57 +0300

    Update Actions workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
 'use strict';
 
-var gulp = require('gulp');
-var debug = require('gulp-debug');
-var del = require('del');
-var sourcemaps = require('gulp-sourcemaps');
-var terser = require('gulp-terser');
-var concat = require('gulp-concat');
-var gulpIf = require("gulp-if");
+const fs = require('fs');
+const gulp = require('gulp');
+const debug = require('gulp-debug');
+const del = require('del');
+const sourcemaps = require('gulp-sourcemaps');
+const terser = require('gulp-terser');
+const concat = require('gulp-concat');
+const gulpIf = require("gulp-if");
 
 const isProd = !!(!process.env.NODE_ENV || process.env.NODE_ENV === "production");
 const isDev = !isProd;
 console.warn("Current mode is " + (isDev ? "development" : "production"));
 
-const DIST_DIR = "docs";
+const DIST_DIR = "dist";
 
 var paths = {
     js: [
-        "three.js",
-        "GLTFLoader.js",
-        "OrbitControls.js",
-        "RoomEnvironment.js",
-        "index.js"
+        "src/WebGL.js",
+        "src/three.js",
+        "src/GLTFLoader.js",
+        "src/OrbitControls.js",
+        "src/RoomEnvironment.js",
+        "src/index.js"
     ],
     copy: [
         "assets/*.*"
@@ -31,6 +33,11 @@ gulp.task('cleanDist', function () {
     return del(DIST_DIR+'/**/*',{force:true});
 });
 
+gulp.task("copyAssets", function () {
+    return gulp.src(paths.copy)
+        .pipe(gulpIf(isDev, debug({title: 'copyAssets:'})))
+        .pipe(gulp.dest(DIST_DIR));
+});
 
 gulp.task('uglifyjs', function () {
     return gulp.src(paths.js)
@@ -43,4 +50,4 @@ gulp.task('uglifyjs', function () {
         .pipe(gulp.dest(DIST_DIR));
 });
 
-gulp.task("default", gulp.series("uglifyjs"));
+gulp.task("default", gulp.series("cleanDist", gulp.parallel("copyAssets", "uglifyjs")));
