45298c6530cf517c0fd5ca95dbeb95fab8d0174e
Max Aniskovich <50804798+m-a-aniskovich@users.noreply.github.com>
2023-08-22 19:12:46 +0300

    Add light sources

--- src/index.js
+++ src/index.js
@@ -26,13 +26,11 @@ function init(file) {
     );
 
     renderer = WebGL.isWebGLAvailable() ? new THREE.WebGLRenderer({antialias: true}) : new THREE.CanvasRenderer();
-
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(container.offsetWidth, container.offsetHeight);
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.toneMapping = THREE.ACESFilmicToneMapping
     renderer.toneMappingExposure = .4;
-
     container.appendChild(renderer.domElement);
 
     const environment = new THREE.RoomEnvironment();
@@ -43,9 +41,33 @@ function init(file) {
     scene.environment = pmremGenerator.fromScene( environment ).texture;
     environment.dispose();
 
+    hlight = new THREE.AmbientLight (0x404040,100);
+    scene.add(hlight);
+    
+    directionalLight = new THREE.DirectionalLight(0xffffff,100);
+    directionalLight.position.set(0,1,0);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+    
+    light = new THREE.PointLight(0xc4c4c4,10);
+    light.position.set(0,300,500);
+    scene.add(light);
+    
+    light2 = new THREE.PointLight(0xc4c4c4,10);
+    light2.position.set(500,100,0);
+    scene.add(light2);
+    
+    light3 = new THREE.PointLight(0xc4c4c4,10);
+    light3.position.set(0,100,-500);
+    scene.add(light3);
+    
+    light4 = new THREE.PointLight(0xc4c4c4,10);
+    light4.position.set(-500,300,500);
+    scene.add(light4);
+
     controls = new THREE.OrbitControls (camera, renderer.domElement);
     controls.enableGizmos = false;
-    controls.target.set(0, 0, 0);
+    controls.target.copy(model.position);
     controls.update();
 
     window.addEventListener('resize', onWindowResize);
