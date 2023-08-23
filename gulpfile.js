6057fd3470702c23868d2b7a94840b3e57baadda
aniskovich <m.a.aniskovich@gmail.com>
2023-08-23 21:29:25 +0300

    Add CSS

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const terser = require('gulp-terser');
 const concat = require('gulp-concat');
 const gulpIf = require("gulp-if");
+const cleanCSS = require('gulp-clean-css');
 
 const isProd = !!(!process.env.NODE_ENV || process.env.NODE_ENV === "production");
 const isDev = !isProd;
@@ -24,6 +25,9 @@ var paths = {
         "src/RoomEnvironment.js",
         "src/index.js"
     ],
+    css: [
+       "src/viewer.css"
+    ],
     copy: [
         "assets/*.*"
     ]
@@ -38,16 +42,26 @@ gulp.task("copyAssets", function () {
         .pipe(gulpIf(isDev, debug({title: 'copyAssets:'})))
         .pipe(gulp.dest(DIST_DIR));
 });
+gulp.task('minify-css', () => {
+    return gulp.src(paths.css)
+        .pipe(gulpIf(isDev, sourcemaps.init()))
+        .pipe(concat('load3D.css'))
+        .pipe(gulpIf(isDev, debug({title: 'css_concat:'})))
+        .pipe(cleanCSS())
+        .pipe(gulpIf(isDev, debug({title: 'css_minify:'})))
+        .pipe(gulpIf(isDev, sourcemaps.write()))
+        .pipe(gulp.dest(DIST_DIR));
+});
 
 gulp.task('uglifyjs', function () {
     return gulp.src(paths.js)
         .pipe(gulpIf(isDev, sourcemaps.init()))
-        .pipe(terser({ output: {comments: false} }))
-        .pipe(gulpIf(isDev, debug({title: 'js_min:'})))
         .pipe(concat('load3D.js'))
-        .pipe(gulpIf(isDev, sourcemaps.write()))
         .pipe(gulpIf(isDev, debug({title: 'js_concat:'})))
+        //.pipe(terser({ output: {comments: false} }))
+        .pipe(gulpIf(isDev, debug({title: 'js_min:'})))
+        .pipe(gulpIf(isDev, sourcemaps.write()))
         .pipe(gulp.dest(DIST_DIR));
 });
 
-gulp.task("default", gulp.series("cleanDist", gulp.parallel("copyAssets", "uglifyjs")));
+gulp.task("default", gulp.series("cleanDist", gulp.parallel("copyAssets", "uglifyjs", "minify-css")));
